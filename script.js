bbd6050ae74acbff6aabbc11a0a9073fc059af4c
yudayuda78 <ariefyudha78@gmail.com>
2022-05-02 15:55:29 +0700

    menambah document.queryselector

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const deletebuttons = document.querySelectorAll("[data-delete]");
+const clearbuttons = document.querySelectorAll("[data-all-clear]");
+const equalbuttons = document.querySelectorAll("[data-equal]");
+const previousOperandText = document.querySelectorAll("[data-previous-operand]");
+const currentOperandText = document.querySelectorAll("[data-current-operand]");
+console.log(operationButtons);
