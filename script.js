fa8c23116196d3d5d5390ec75dcdc0efa6ca1180
yudayuda78 <ariefyudha78@gmail.com>
2022-05-03 14:29:16 +0700

    menambahkan input angka dan clear

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 
-
-
-
-
-
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
-const deletebuttons = document.querySelectorAll("[data-delete]");
-const clearbuttons = document.querySelectorAll("[data-all-clear]");
-const equalbuttons = document.querySelectorAll("[data-equal]");
-const previousOperandText = document.querySelectorAll("[data-previous-operand]");
-const currentOperandText = document.querySelectorAll("[data-current-operand]");
+const deletebuttons = document.querySelector("[data-delete]");
+const clearbuttons = document.querySelector("[data-all-clear]");
+const equalbuttons = document.querySelector("[data-equal]");
+const previousOperandText = document.querySelector("[data-previous-operand]");
+const currentOperandText = document.querySelector("[data-current-operand]");
+
+let display = document.querySelector(".output");
+let buttons = Array.from(numberButtons);
+let operandButtons = Array.from(operationButtons);
+
+buttons.map( button =>{
+    button.addEventListener('click', (e) => {
+        console.log(e.target);
+        console.log(e.target.innerText);
+        switch(e.target.innerText){
+            default:
+                previousOperandText.innerText += e.target.innerHTML;
+        }
+    })
+})
+
+operandButtons.map(operandButton =>{
+    operandButton.addEventListener('click', (e) => {
+        console.log(e.target.innerText);
+        previousOperandText.innerHTML += e.target.innerHTML;
+    })
+})
 
 
-numberButtons.forEach(function(){
 
-});
+clearbuttons.addEventListener('click', (e) => {
+    previousOperandText.innerHTML = "";
+})
 
 
 
